bb068fcfbd186c82ff80cf93a7301b9ef29a61c7
SueA16 <SueA16@qq.com>
2024-02-18 22:01:41 +0800

    init for project

---
+++ src/main/java/com/cqnu/pan/FastPanApplication.java
@@ -0,0 +1,13 @@
+package com.cqnu.pan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastPanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastPanApplication.class, args);
+    }
+
+}
